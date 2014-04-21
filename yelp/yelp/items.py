f5c3ebe3e3f550625c62ada3bbb5ee5912c549a8
Sam Fung <sam.fung@gmail.com>
2014-04-20 21:23:47 -0700

    Add spider.

--- yelp/yelp/items.py
+++ yelp/yelp/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 
-class YelpItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+
+class EntityItem(Item):
+    url = Field()
+    title = Field()
