18d7fc0f6b69013f00aa73c407d201656b63a605
Sam Fung <sam.fung@gmail.com>
2014-04-23 20:06:42 -0700

    Add settings.

--- crawl/crawl/settings.py
+++ crawl/crawl/settings.py
@@ -11,5 +11,13 @@ BOT_NAME = 'crawl'
 SPIDER_MODULES = ['crawl.spiders']
 NEWSPIDER_MODULE = 'crawl.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'crawl (+http://www.yourdomain.com)'
+# Turn off cookies.  We don't want sites to track us.
+COOKIES_ENABLED = False
+
+# Random delay from 0.5 and 1.5 * DOWNLOAD_DELAY
+RANDOMIZE_DOWNLOAD_DELAY = True
+DOWNLOAD_DELAY = 1
+
+# Use a real browser like user agent rather than the default scrappy user agent.
+USER_AGENT = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) '
+              'Chrome/35.0.1916.47 Safari/537.36')
